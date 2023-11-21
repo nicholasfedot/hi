7d1f2881bc04b7272de1a23fcace1b8f4e8864a7
nicholasfedot <151592122+nicholasfedot@users.noreply.github.com>
2023-11-21 13:00:06 +1100

    Add files via upload

---
+++ hi.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class i:Form
+{
+	static void Main()
+	{
+		Application.Run(new i());
+	}
+	Public Button i;
+	Static void i()
+	{
+		i=new Button();
+	}
+}
